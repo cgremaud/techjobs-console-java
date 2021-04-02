a722a006667493528d29ec33a871fe8f92ca4936
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-02 14:48:24 -0500

    printJobs prints error if no jobs found

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -111,13 +111,15 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        for (HashMap<String, String> record : someJobs) {
-//            System.out.println(record.values());
+        //TODO figure out how to check if someJobs is empty
+        if (someJobs.size() == 0) {
+            System.out.println("Error, no jobs found");
+        }
+        for (HashMap<String, String> job : someJobs) {
             System.out.println("**********");
-            for (Map.Entry<String, String> entry: record.entrySet()) {
+            for (Map.Entry<String, String> entry: job.entrySet()) {
                 System.out.println(entry.getKey() + ": " + entry.getValue());
             }
-//            System.out.println("**********");
         }
 
     }
